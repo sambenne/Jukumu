2a4c5f1de840fff3235853ffa9ebc2cc6c54057f
Sam Bennett <bennettsst@gmail.com>
2015-05-24 16:17:24 +0100

    Added config file and added version to composer

---
+++ src/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * This file is part of Jukumu,
+     * A Role and Permission management solution for Laravel.
+     *
+     * @license MIT
+     * @package sbenentt\jukumu
+     */
+
+    return [
+
+    ];
