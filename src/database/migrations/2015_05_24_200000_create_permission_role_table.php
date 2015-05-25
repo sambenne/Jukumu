72088fbe72553dfd1d2b5bc8ef133bbcbf3ee47d
Sam Bennett <bennettsst@gmail.com>
2015-05-25 15:43:56 +0100

    Changed migration name

--- src/database/migrations/2015_05_24_200000_create_permission_role_table.php
+++ src/database/migrations/2015_05_24_200000_create_permission_role_table.php
@@ -9,7 +9,7 @@
     use Illuminate\Database\Schema\Blueprint;
     use Illuminate\Database\Migrations\Migration;
 
-    class CreateRolePermissionsTable extends Migration {
+    class CreatePermissionRoleTable extends Migration {
 
         /**
          * Run the migrations.
